e1b04937cdef4cd5b099a064a2d04731e9656556
MinNaingKoKo <minnaingkoko2001@gmail.com>
2024-06-24 11:47:49 +0630

    company remove

--- src/components/Company/CompanyTextData.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let text: any, value: any;
-</script>
-
-<div class="flex pl-[30px] pr-[30px] w-[100%] justify-between text-[14px]">
-	<label class="w-[130px]" for="companyName">{text}</label>
-	<div class="w-[160px]">{value}</div>
-</div>
